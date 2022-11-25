9f2bc158ce15c9855a8468a35f08e2a6142040f8
ayaan09 <hanzalah.choudhury@gmail.com>
2022-11-25 23:54:54 +0800

    make it better

--- server.js
+++ server.js
@@ -32,10 +32,18 @@ app.use(bodyParser.json());
 app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.xml());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+  });
 app.get('/', (req,res)=>{
   res.json("Server Start")
  })
+
+ 
 
 mongoose.set('useUnifiedTopology', true); // get rid of the Deprecation Warning
 const db = process.env.DATABASE
