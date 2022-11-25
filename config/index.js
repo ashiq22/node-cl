ed440416ae7408a3ba3eaf5d4d0b5185d2500d99
ayaan09 <hanzalah.choudhury@gmail.com>
2022-11-25 23:18:08 +0800

    make it better

--- config/index.js
+++ config/index.js
@@ -4,7 +4,5 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI,
-  MONGO_DB_NAME: process.env.MONGO_DB_NAME,
   JWT_SECRET: process.env.JWT_SECRET
 };
