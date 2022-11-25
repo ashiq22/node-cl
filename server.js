7c103df013170ae8fe82589e34f7101b74c35e0d
ayaan09 <hanzalah.choudhury@gmail.com>
2022-11-25 23:47:03 +0800

    make it better

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cors = require("cors"); //enable CORS
 require('body-parser-xml')(bodyParser);
 const port = process.env.PORT || 8003;
 const corsOptions ={
-  origin:"http://localhost:3000", 
+  origin:"*", 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
