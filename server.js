bb13c55b1d461347adfe6d1d0d48e9a70dea8e2c
ashiq22 <ashiq_22@hotmail.com>
2022-11-29 18:36:39 +0530

    cors

--- server.js
+++ server.js
@@ -40,7 +40,13 @@ app.use(function(req, res, next) {
   next();
   });
 app.get('/', (req,res)=>{
-  res.json("Server Start")
+  res.json("Server Start");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
  })
 
  
