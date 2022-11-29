8fcb449bdcc57841f5b0f96b76114c8ff99e79d4
ashiq22 <ashiq_22@hotmail.com>
2022-11-29 19:10:43 +0530

    server file

--- server.js
+++ server.js
@@ -33,20 +33,10 @@ app.use(cors());
 app.options('*', cors());
 app.use(bodyParser.xml());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
   });
 app.get('/', (req,res)=>{
-  res.json("Server Start");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "*")
-res.setHeader("Access-Control-Allow-Credentials", "true");
-res.setHeader("Access-Control-Max-Age", "1800");
-res.setHeader("Access-Control-Allow-Headers", "content-type");
-res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+  res.json("Server Start")
  })
 
  
