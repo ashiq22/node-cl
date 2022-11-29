2e21bfad6b99ded631c47221643c14bbad40303d
ashiq22 <83548540+ashiq22@users.noreply.github.com>
2022-11-30 00:05:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,10 @@ app.use(function(req, res, next) {
   });
 app.get('/', (req,res)=>{
   res.header("Access-Control-Allow-Origin", "true");
-  
+   res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 
+    Accept, x-client-key, x-client-token, x-client-secret, Authorization");
   
   res.json("Server Started")
  })
